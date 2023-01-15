3d680e4406d391492429a6b45775fab59891516c
BrianLucy <116689598+BrianLucy@users.noreply.github.com>
2023-01-15 18:13:50 -0500

    updated js script for users to enter choices

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,36 @@
 // Assignment code here
 
+//define generatePassword function
+function generatePassword(){
 
+//define local variable  
+var pwResults = "";
+//user can choose length of password 8-128
+  var characterLength = window.prompt("Please choose the length of your password. \n Password length must be between 8 and 128 charcters long. \n Input that value in the box and clock OK."0:
+//user input is a string and needs to be converted to interger
+var pwLength = parseInt(characterLength);
+  
+if (pwLength > && pwLength < 129) {
+  var lowerCase = window.confirm(
+    "do you want to include lowercase letters in your password?\nClick OK to include or cancel to exclude.");
+  var uperCase - window.confirm(
+    "do you want to include uppercase letters in your password?\nCLick OK to include or cancel to exclude.");
+  var numericVal - window.confirm(
+    "do you want to include numbers in your password?\nCLick OK to include or cancel to exclude.");
+  var specChar - window.confirm(
+    "do you want to include special characters in your password?\nCLick OK to include or cancel to exclude.");                        
+} else {
+  //alert pops up if user enters number less than 8 or more than 128
+  window.alert("that is an invalid entry.  Select a length between 8 and 128");
+  return generatePassword();
+}
+  return pwResults();
+}
+
+  
+  
+  
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
